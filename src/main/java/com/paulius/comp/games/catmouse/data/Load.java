0d3eda75541377609a47737e28b416ef69e35bc1
Boss7757 <36923250+Boss7757@users.noreply.github.com>
2018-12-14 12:39:51 +0200

    v8

--- src/main/java/com/paulius/comp/games/catmouse/data/Load.java
+++ src/main/java/com/paulius/comp/games/catmouse/data/Load.java
@@ -2,9 +2,7 @@ package com.paulius.comp.games.catmouse.data;
 
 import com.paulius.comp.games.catmouse.model.MapPoint;
 
-import static com.paulius.comp.games.catmouse.model.PointType.CAT;
-import static com.paulius.comp.games.catmouse.model.PointType.EMPTY;
-import static com.paulius.comp.games.catmouse.model.PointType.MOUSE;
+import static com.paulius.comp.games.catmouse.model.PointType.*;
 
 /**
  * Created by Admin on 2018-12-14.
@@ -17,7 +15,7 @@ public interface Load {
             {
                     {new MapPoint(MOUSE, 0, 0), new MapPoint(EMPTY, 1, 0), new MapPoint(EMPTY, 2, 0), new MapPoint(EMPTY, 3, 0), new MapPoint(EMPTY, 4, 0)},
                     {new MapPoint(EMPTY, 0, 1), new MapPoint(EMPTY, 1, 1), new MapPoint(EMPTY, 2, 1), new MapPoint(EMPTY, 3, 1), new MapPoint(EMPTY, 4, 1)},
-                    {new MapPoint(EMPTY, 0, 2), new MapPoint(EMPTY, 1, 2), new MapPoint(EMPTY, 2, 2), new MapPoint(EMPTY, 3, 2), new MapPoint(EMPTY, 4, 2)},
+                    {new MapPoint(EMPTY, 0, 2), new MapPoint(EMPTY, 1, 2), new MapPoint(WALL, 2, 2), new MapPoint(EMPTY, 3, 2), new MapPoint(EMPTY, 4, 2)},
                     {new MapPoint(EMPTY, 0, 3), new MapPoint(EMPTY, 1, 3), new MapPoint(EMPTY, 2, 3), new MapPoint(EMPTY, 3, 3), new MapPoint(EMPTY, 4, 3)},
                     {new MapPoint(EMPTY, 0, 4), new MapPoint(EMPTY, 1, 4), new MapPoint(EMPTY, 2, 4), new MapPoint(EMPTY, 3, 4), new MapPoint(CAT, 4, 4)},
             };
--- unitTest/src/CatMoveTest.java
+++ unitTest/src/CatMoveTest.java
@@ -1,6 +1,13 @@
 /**
  * Created by Admin on 2018-12-14.
  */
+
+import static org.junit.Assert.assertTrue;
+
+import com.paulius.comp.games.catmouse.data.MapLoader;
+import java.com.paulius.comp.games.catmouse.model.MazeMap;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,10 +15,21 @@ import org.junit.Test;
 public class CatMoveTest {
     @Before
     public void init() {
+        MapPoint[][] m = new MapPoint[][]{
+                {new MapPoint(MOUSE, 0, 0), new MapPoint(EMPTY, 1, 0), new MapPoint(EMPTY, 2, 0), new MapPoint(EMPTY, 3, 0), new MapPoint(EMPTY, 4, 0)},
+                {new MapPoint(EMPTY, 0, 1), new MapPoint(EMPTY, 1, 1), new MapPoint(EMPTY, 2, 1), new MapPoint(EMPTY, 3, 1), new MapPoint(EMPTY, 4, 1)},
+                {new MapPoint(EMPTY, 0, 2), new MapPoint(EMPTY, 1, 2), new MapPoint(WALL, 2, 2), new MapPoint(EMPTY, 3, 2), new MapPoint(EMPTY, 4, 2)},
+                {new MapPoint(EMPTY, 0, 3), new MapPoint(WALL, 1, 3), new MapPoint(CAT, 2, 3), new MapPoint(EMPTY, 3, 3), new MapPoint(EMPTY, 4, 3)},
+                {new MapPoint(EMPTY, 0, 4), new MapPoint(WALL, 1, 4), new MapPoint(EMPTY, 2, 4), new MapPoint(EMPTY, 3, 4), new MapPoint(EMPTY, 4, 4)},
+        };
+
 
     }
-        @Test
-        public void successCases0() {}
 
+    @Test
+    public void successCases0() {
+   Assert.assertEquals(MapPoint(EMPTY, 3, 3), MazeMap.calcNextCatMove(m));
     }
+
+}
 
