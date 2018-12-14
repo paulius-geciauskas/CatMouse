f6ff1e7c9609930d6786430e080508dcb2783228
Boss7757 <36923250+Boss7757@users.noreply.github.com>
2018-12-14 10:00:16 +0200

    v6

---
+++ unitTest/src/CatMoveTest.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Admin on 2018-12-14.
+ */
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class CatMoveTest {
+    @Before
+    public void init() {
+
+    }
+        @Test
+        public void successCases0() {}
+
+    }
+
